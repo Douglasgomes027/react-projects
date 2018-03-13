aa720a16d0370eb1b9e944d92b313b0ac62aced1
Douglas Gomes <douglasgomes027@gmail.com>
2018-03-13 18:31:20 -0300

    done components

--- weather-application/src/components/Weather.js
+++ weather-application/src/components/Weather.js
@@ -1,17 +1,13 @@
-import React,{Component} from 'react';
+import React from 'react';
 
-class Weather extends Component{
-    render(){
-        return(
-            <div>
-                {this.props.temperature}
-                {this.props.city}
-                {this.props.country}
-                {this.props.humidity}
-                {this.props.description}
-            </div>
-        );
-    }
-}
+const Weather = props=>(
+    <div>
+        {props.city && props.country && <p>Location: {props.city},{props.country}</p>}
+        {props.temperature && <p>Temperature: {props.temperature}</p>}
+        {props.humidity && <p>Humidity: {props.humidity}</p>}
+        {props.description && <p>Description: {props.description}</p>}
+        {props.error && <p>{props.error}</p>}                               
+    </div>
+)
 
 export default Weather;
